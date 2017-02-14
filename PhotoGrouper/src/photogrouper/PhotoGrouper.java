91bc8d05c13ae8e3b1bfb3998557c8497246043f
onder <onder@onder-PC>
2017-02-14 19:43:33 +0200

    first commit

---
+++ PhotoGrouper/src/photogrouper/PhotoGrouper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package photogrouper;
+
+/**
+ *
+ * @author onder
+ */
+public class PhotoGrouper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Photo Grouper App Started");
+    }
+    
+}
